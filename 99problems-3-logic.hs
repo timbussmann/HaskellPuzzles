4d07aeadf0e5fb411d3339c38de6d05c7b40f127
Tim Bussmann <timbussmann@users.noreply.github.com>
2018-12-03 23:21:32 +0100

    problem 47 & 48

--- 99problems-3-logic.hs
+++ 99problems-3-logic.hs
@@ -32,3 +32,23 @@ table :: (Bool -> Bool -> Bool) -> [[Bool]]
 
 -- using list comprehensions:
 table f = [[a, b, f a b] | a <- [True, False], b <- [True, False]]
+
+
+problem47 = TestList [
+    TestCase $ assertEqual "" [
+        [True, True, True], 
+        [True, False, True], 
+        [False, True, False], 
+        [False, False, False]] 
+            (table (\a b -> a `and'` (a `or'` not b)))
+    ]
+
+
+problem48 = TestList[
+    TestCase $ assertEqual "" ["000","001","010","011","100","101","110","111"] (gray 3)
+    ]
+
+gray :: Int -> [String]
+gray 0 = [""]
+gray n = let prev = gray (n - 1) in map ('0':) prev ++ map ('1':) prev
+-- gray n = foldr (\s acc -> ("0" ++ s):("1" ++ s):acc) [] $ gray (n-1)
