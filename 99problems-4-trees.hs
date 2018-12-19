c9250284e095732f69b2377dd674b0562380cc07
Tim Bussmann <timbussmann@users.noreply.github.com>
2018-12-19 22:58:10 +0100

    add problem 56

--- 99problems-4-trees.hs
+++ 99problems-4-trees.hs
@@ -26,3 +26,19 @@ cbalTree 0 = [Empty]
 cbalTree n = let (q, r) = (n - 1) `divMod` 2 in [Branch 'x' left right | i <- [q .. (q + r)],
                                                                    left <- cbalTree i,
                                                                    right <- cbalTree (n - i - 1)]
+
+
+problem56 = TestList [
+    TestCase $ assertEqual "when tree is not symmetric" False (symmetric (Branch 'x' (Branch 'x' Empty Empty) Empty)),
+    TestCase $ assertEqual "when tree is symmetric" True (symmetric (Branch 'x' (Branch 'x' Empty Empty) (Branch 'x' Empty Empty))),
+    TestCase $ assertEqual "when complex tree is symmetric" True (symmetric (Branch 'x' (Branch 'x' (Branch 'x' Empty Empty) Empty) (Branch 'x' Empty  (Branch 'x' Empty Empty))))
+    ]
+
+symmetric :: Tree Char -> Bool
+symmetric Empty = True
+symmetric (Branch _ l r) = mirror l r
+
+mirror :: Tree a -> Tree a -> Bool
+mirror Empty Empty = True
+mirror (Branch _ l1 r1) (Branch _ l2 r2) = mirror l1 r2 && mirror r1 l2 -- ignore values for now
+mirror _ _ = False
